78f296dbb8db19314a3608e3a5fc9fc87120dd94
Robert Haupt <robert.haupt@infinitas.de>
2026-01-06 11:26:59 +0100

    WebsiteCustomer: Add multi-language support and footer pages
    - Add SUPPORTED_LANGUAGES and DEFAULT_LANGUAGE constants
    - Update footer with Contact, Privacy, Imprint links
    - Add Contact, Privacy, Imprint pages (English)
    - Add Resources.resx for English UI strings
    - Create language-based screenshot folders (de/en/es/fr/pt)

--- src/WebsiteCustomer/Pages/Contact.cshtml.cs
+++ src/WebsiteCustomer/Pages/Contact.cshtml.cs
@@ -5,12 +5,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace WebsiteCustomer;
 
 /// <summary>
-/// Aim: Page model for the Privacy Policy page.
+/// Aim: Page model for the Contact page.
 /// </summary>
-public class PrivacyModel : PageModel
+public class ContactModel : PageModel
 {
     public void OnGet()
     {
     }
 }
-
