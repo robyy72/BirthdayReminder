d9cbb7e3fec2a509b111b001655bd73a44c236fe
Robert <robert.haupt@parequin.com>
2026-01-17 18:36:21 +0100

    Add ApiMobile deployment pipelines and rename workflow files
    - Add deploy-api-dev.yml for ApiMobile Dev deployments
    - Add deploy-api-prod.yml for ApiMobile Prod deployments
    - Rename deploy-admin.yml to deploy-admin-prod.yml
    - Rename deploy-customer.yml to deploy-customer-prod.yml

--- src/ApiMobile/Program.cs
+++ src/ApiMobile/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.IdentityModel.Tokens;
 using ApiMobile;
 #endregion
 
+// Initial deployment via GitHub Actions pipeline
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services
